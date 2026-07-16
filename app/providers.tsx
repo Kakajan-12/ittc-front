0e3a9c77c082f18638f196fefb96d5321485d882
Mamagul <mamagul.gapurova@mail.ru>
2026-07-16 14:52:20 +0500

    fix meta

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,14 +2,11 @@
 
 import { useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { AntdRegistry } from "@ant-design/nextjs-registry";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <AntdRegistry>{children}</AntdRegistry>
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
 }
